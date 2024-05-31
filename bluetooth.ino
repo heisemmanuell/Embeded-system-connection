dd03a9a5a214fe9b27d283ce00e050a811d6863f
Chidindu Emmanuel <chidinduemma123@gmail.com>
2024-05-31 19:10:41 +0100

    bluetooth connection with arduino and proteus

---
+++ bluetooth.ino
@@ -0,0 +1,29 @@
+String text;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+while(Serial.available())
+{
+delay(10);
+char c = Serial.read();
+text += c;
+}
+if (text.length() > 0)
+{
+  Serial.println(text);
+  if(text == "on")
+  {
+    digitalWrite(13, HIGH);
+  }
+  if(text == "off"){
+    digitalWrite(13, LOW);
+  }
+  text=""; }
+}
+
